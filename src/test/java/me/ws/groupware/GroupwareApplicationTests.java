70bd6bfd35b683e624d4ca0c2a51c4661f04ba30
woosung-Lee <64631529+woosung-Lee@users.noreply.github.com>
2022-06-05 13:24:27 +0900

    Initial commit

---
+++ src/test/java/me/ws/groupware/GroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ws.groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
