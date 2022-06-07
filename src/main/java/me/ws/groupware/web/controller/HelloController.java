6242798fbee2a91824ad62fea533cd370361ac02
woosung-Lee <64631529+woosung-Lee@users.noreply.github.com>
2022-06-07 14:19:51 +0900

    thymeleaf hellowold 테스트

---
+++ src/main/java/me/ws/groupware/web/controller/HelloController.java
@@ -0,0 +1,14 @@
+package me.ws.groupware.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        return "/index.html";
+    }
+}
