935b781d04cbbbc25f628ef730a78e9ca05fe1a3
woosung-Lee <64631529+woosung-Lee@users.noreply.github.com>
2022-06-08 10:03:52 +0900

    member, payments 도메인 설정 중

--- src/main/java/me/ws/groupware/domain/Member.java
+++ src/main/java/me/ws/groupware/domain/Member.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -19,5 +21,7 @@ public class Member {
     private Address address;
 
     private MemberRole memberRole;
+
+    private List<Payments> payments = new ArrayList<>();
 
 }
--- src/main/java/me/ws/groupware/domain/Payments.java
+++ src/main/java/me/ws/groupware/domain/Payments.java
@@ -1,4 +1,17 @@
 package me.ws.groupware.domain;
 
+import lombok.Getter;
+import org.hibernate.annotations.Tables;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="payments")
+@Getter
 public class Payments {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title; //결제할 문서 제목
 }
